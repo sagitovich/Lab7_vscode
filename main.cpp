8111bf0a257bfeea378f9c5fa7b32ed0557880dd
Alex <a.a.sagitovich@gmail.com>
2022-11-08 23:36:26 +0200

    first upd

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a;
+    cout << "Enter number: ";
+    cin >> a;
+    cout << "Result: " << a * a << endl;
+
+    return 0;
+}
